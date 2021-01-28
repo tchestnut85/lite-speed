0e50f03178e65db9855cf02e21877280e59b8aba
tchestnut85 <tchestnut85@gmail.com>
2021-01-27 19:32:19 -0500

    Setup App file with components using React Router.

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,51 @@
-import React from "react";
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+
+import { ApolloClient } from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+import Chat from './components/Chat';
+import Course from './pages/Course';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
+import Profile from './pages/Profile';
+import React from 'react';
+import Signup from './pages/Signup';
+
+const client = new ApolloClient({
+  request: (operation) => {
+    const token = localStorage.getItem('id_token');
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    });
+  },
+  uri: '/graphql',
+});
 
 function App() {
   return (
-    <div className="App">
-      <p>Hello World!</p>
-    </div>
+    <ApolloProvider client={client}>
+      <Router>
+        <div className="flex-column justify-flex-start min-100-vh">
+          <Header />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/signup" component={Signup} />
+              <Route exact path="/profile" component={Profile} />
+              <Route exact path='/course' component={Course} />
+              <Route component={NotFound} />
+            </Switch>
+            <Chat />
+          </div>
+          <Footer />
+        </div>
+      </Router>
+    </ApolloProvider>
   );
 }
 
