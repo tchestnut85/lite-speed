f0b7fa802a16d156f6d9c8c2217ac9822036ef90
Spiewak <bspiewak6@gmail.com>
2021-01-29 13:34:43 -0500

    updated landing page with background image, tagline and subheader

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -10,6 +10,7 @@ function LandingPage() {
     
     return (
     <div className='big-image'>
+        <div className="overlay">
         <div className='landing-nav'>
             <Link to="/" className="landing-nav-title">
             <h1>Warp <i>Speed</i></h1>
@@ -17,22 +18,24 @@ function LandingPage() {
             
             <nav className='landing-navbar'>
                 {Auth.loggedIn() ? (
-                <>
+                    <>
                     <Link to='/course' className='landing-list-item'>Courses</Link>
                     <Link to='/profile' className='landing-list-item'>Profile</Link>
                     <a href='/' onClick={logout} className='landing-list-item'>Logout</a>
                 </>
             ) : (
-                    <>
+                <>
                     <Link to='/login' className='landing-list-item'>Login</Link>
                     <Link to='/signup' className='landing-list-item'>Signup</Link>
                     </>
                 )}
             </nav>
         </div>
-        <div className="hero-text">
-            Education can be the pathway to your next great step. 
-            Let us help open up the door for you to get a leg up on the competition in Warp Speed!
+            <div>
+                {/* <p className="hero-text">Learning is the greatest pathway to your next career.</p>  */}
+                <p className="hero-text">Learn. Achieve. Connect. </p> 
+                <p className="hero-subtext">An educational platform that helps you get a leg up on the competition in the speed of light!</p>
+            </div>
         </div>
     </div>
     );
