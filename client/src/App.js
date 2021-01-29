ec45642e1cd872d7c5fa5748847da586b59cdbd8
Spiewak <bspiewak6@gmail.com>
2021-01-28 21:51:35 -0500

    updated landing page with full width image and navbar

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from '@apollo/react-hooks';
+import Auth from './utils/auth';
 // import Chat from './components/Chat';
 // import Course from './pages/Course';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Home from './pages/Home';
 // import Login from './pages/Login';
+import LandingPage from './pages/LandingPage';
 // import NotFound from './pages/NotFound';
 // import Profile from './pages/Profile';
 import React from 'react';
@@ -31,10 +33,12 @@ function App() {
     <ApolloProvider client={client}>
       <Router>
         <div className="flex-column justify-flex-start min-100-vh">
+        {Auth.loggedIn() ? (
+          <>
           <Header />
-          <div className="container">
+          <div className="container">  
             <Switch>
-              <Route exact path="/" component={Home} />
+              <Route exact path="/home" component={Home} />
               {/* <Route exact path="/login" component={Login} /> */}
               {/* <Route exact path="/signup" component={Signup} /> */}
               {/* <Route exact path="/profile" component={Profile} /> */}
@@ -44,6 +48,12 @@ function App() {
             {/* <Chat /> */}
           </div>
           <Footer />
+          </>
+        ) : (
+              <>
+              <Route exact path="/" component={LandingPage} />
+              </>
+        )}
         </div>
       </Router>
     </ApolloProvider>
---
+++ client/src/pages/LandingPage.js
@@ -0,0 +1,38 @@
+import Auth from '../utils/auth';
+import { Link } from 'react-router-dom';
+import React from 'react';
+
+function LandingPage() {
+    const logout = event => {
+        event.preventDefault();
+        Auth.logout();
+    };
+    
+    return (
+            
+    <div className='big-image'>
+        <div className='nav-wrap'>
+            <Link to="/" className="nav-title">
+            <h1>Warp <i>Speed</i></h1>
+            </Link>
+            
+            <nav className='navbar'>
+                {Auth.loggedIn() ? (
+                <>
+                    <Link to='/course' className='list-item'>Courses</Link>
+                    <Link to='/profile' className='list-item'>Profile</Link>
+                    <a href='/' onClick={logout} className='list-item'>Logout</a>
+                </>
+            ) : (
+                    <>
+                    <Link to='/login' className='list-item'>Login</Link>
+                    <Link to='/signup' className='list-item'>Signup</Link>
+                    </>
+                )}
+            </nav>
+        </div>
+    </div>
+    );
+  }
+
+  export default LandingPage;
