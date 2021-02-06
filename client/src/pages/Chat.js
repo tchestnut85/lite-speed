cc0c3741b705d4e359774d83500c93759d699ff9
Peyton Schlafley <pschlafley0@gmail.com>
2021-02-06 09:51:09 -0500

    created chat.js file for chatroom feature

---
+++ client/src/pages/Chat.js
@@ -0,0 +1,25 @@
+// import React, { useEffect } from 'react';
+// // import { Link } from 'react-router-dom';
+// import { Widget, addResponseMessage } from 'react-chat-widget';
+// import 'react-chat-widget/lib/styles.css';
+// import { useQuery } from '@apollo/react-hooks';
+// import { QUERY_ME } from '../utils/queries';
+// import Auth from '../utils/auth';
+
+
+// function Chat() {
+//     const { loading, data } = useQuery(QUERY_ME);
+
+//     const userData = data?.me || {};
+//     console.log(userData);
+//     const token = Auth.getToken();
+//     console.log(token);
+
+
+
+//     return (
+
+//     );
+// };
+
+// export default Chat;
